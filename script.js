ab7473d495cc893513b45327d94b31152b61e459
Charles Afolabi <charles49ja@gmail.com>
2026-01-08 00:53:17 +0100

    introduction to Javascript.Variables and DOM manipulation

---
+++ script.js
@@ -0,0 +1,26 @@
+// console.log("Hello World!")
+// alert("Tinubu is a thief")
+
+// Variables
+
+let info_store1 = 'Yello World'
+let info_store2 = 'Another data'
+
+console.log(info_store1)
+console.log(info_store2)
+
+
+// JAVASCRIPT DOM AND EVENTS
+
+// document.getElementById("title").textContent = "Another text change"
+
+const heading = document.getElementById("title")
+heading.textContent ="We are changing the text"
+heading.style.color = "green"
+
+const button = document.getElementById("btn")
+
+// button.addEventListener("click",function(){
+//    heading.textContent ='You just clicked the button below!' 
+//    heading.style.color ='red'
+// })
