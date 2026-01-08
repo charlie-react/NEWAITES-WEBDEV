85bada8e290adb9e688e662dc6aee8741849a591
Charles Afolabi <charles49ja@gmail.com>
2026-01-08 21:00:24 +0100

    functions in javascript

--- script.js
+++ script.js
@@ -19,8 +19,44 @@ heading.textContent ="We are changing the text"
 heading.style.color = "green"
 
 const button = document.getElementById("btn")
+const navbar_container = document.getElementById("navbar")
+const wrapper = document.getElementById("wrapper")
+console.log(navbar_container)
 
-// button.addEventListener("click",function(){
+function changeStyle(){
 //    heading.textContent ='You just clicked the button below!' 
 //    heading.style.color ='red'
+// navbar_container.style.color = "yellow"
+wrapper.style.backgroundColor = "orange"
+}
+
+// button.addEventListener("click",function(){
+// //    heading.textContent ='You just clicked the button below!' 
+// //    heading.style.color ='red'
+// navbar_container.style.color = "yellow"
+// wrapper.style.backgroundColor = "orange"
 // })
+
+button.addEventListener("click",changeStyle)
+
+// FUNCTIONS
+
+// function greet(){
+//     alert("Good evening")
+// }
+
+// greet()
+
+function addNums(){
+    const total = 5 + 5
+    console.log(total)
+}
+
+addNums()
+
+function subNums(){
+    const result = 20-12
+    console.log(result)
+}
+
+subNums()
